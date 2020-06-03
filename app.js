d6d378b5e676de85976196557bb5afa8a73100d9
Anil Poudyal <anilrunjakote@gmail.com>
2020-06-03 09:25:42 +0545

    Completed UI and created Word List

---
+++ app.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () => {
+    const inputWord = document.querySelector("#input-word");
+    const dispWord = document.querySelector("#word");
+    const time = document.querySelector("#time");
+    const score = document.querySelector("#score");
+    const result = document.querySelector("#result")
+    const words = [
+        "accelerandi", "accelerando", "accelerant", "accelerate",
+        "boundaries", "boundedness", "excelled", "excellence", "excellencies",
+        "excellency", "excellent", "excellently", "excelling", "excels", "excelsior", "except",
+        "excepted", "obsecrate", "obsequent", "obsequies", "obsequious",
+        "obsequiously", "obsequiousness", "obsequy", "observability",
+        "observable", "observables", "observably", "observance",
+        "observances", "observant", "observantly", "observation",
+        "observational", "observationally", "excepting", "exception", "exceptionable",
+        "exceptionably", "exceptional", "exceptionally", "exceptions",
+        "exceptive", "excepts", "excerpt", "exchange", "exchangeable",
+        "exchequer", "exchequers", "bounds", "bounteous", "bounteousness",
+        "bounties", "bountifulness", "nonpolitical", "nonpolitically",
+        "nonpolluting", "nonporous", "nonporousness", "nonpracticing",
+        "nonpregnant", "nonprejudicial", "nonprescription", "nonproductive",
+        "nonprofessional", "nonprofessionals", "nonprofit",
+        "nonprofitable", "nonprofits", "nonproliferation", "accelerations",
+        "accelerative", "accelerator",
+
+    ]
+
+})
